8a04a2ba88913744157259defa7d9146480ca59b
pethanp <epelton@zagmail.gonzaga.edu>
2021-04-22 19:30:36 -0700

    get/set

--- js/IPAddress.js
+++ js/IPAddress.js
@@ -14,13 +14,16 @@ export class IP {
         return this.IPInt();
     }
     // Setter
+    set IPStr() {
+        return this.IPString();
+    }
+    // Method
     setIP(b1, b2, b3, b4) {
         this.b1 = b1;
         this.b2 = b2;
         this.b3 = b3;
         this.b4 = b4;
     }
-    // Method
     IPString() {
         return ""+this.b1+"."+this.b2+"."+this.b3+"."+this.b4;
     }
