65b285532d2fe10d8963ded9cbb457b579132d1c
zeintry <alanpo2000@hotmail.com>
2021-04-30 14:17:25 -0700

    Added labels and edge ids

--- js/main.js
+++ js/main.js
@@ -22,22 +22,22 @@ function startNetwork() {
 
   // create an array of edges
   edges = new vis.DataSet([
-    { from: 1, to: 3, },
-    { from: 1, to: 2 },
-    { from: 1, to: 8 },
-    { from: 2, to: 3 },
-    { from: 2, to: 6 },
-    { from: 3, to: 4 },
-    { from: 4, to: 5 },
-    { from: 5, to: 6 },
-    { from: 5, to: 7 },
-    { from: 6, to: 7 },
-    { from: 6, to: 10 },
-    { from: 7, to: 8 },
-    { from: 7, to: 9 },
-    { from: 7, to: 11 },
-    { from: 8, to: 9 },
-    { from: 9, to: 11 },
+    { id: "1-3", from: 1, to: 3, label: '5'},
+    { id: "1-2", from: 1, to: 2, label: '3'},
+    { id: "1-8", from: 1, to: 8, label: '4'},
+    { id: "2-3", from: 2, to: 3, label: "3"},
+    { id: "2-6", from: 2, to: 6, label: '4'},
+    { id: "3-4", from: 3, to: 4, label: "4"},
+    { id: "4-5", from: 4, to: 5, label: '2'},
+    { id: "5-6", from: 5, to: 6, label: "3"},
+    { id: "5-7", from: 5, to: 7, label: "4"},
+    { id: "6-7", from: 6, to: 7, label: '7'},
+    { id: "6-10", from: 6, to: 10, label: '5'},
+    { id: "7-8", from: 7, to: 8, label: '3'},
+    { id: "7-9", from: 7, to: 9, label: '5'},
+    { id: "7-11", from: 7, to: 11, label: '6'},
+    { id: "8-9", from: 8, to: 9, label: '8'},
+    { id: "9-11", from: 9, to: 11, label: '7'},
   ]);
 
   // Provide the data in the vis format
